db4400dfdaa4a71d018bbb4854c361c422a1b9c4
wspaleniak <w.spaleniak@o2.pl>
2022-11-02 20:47:09 +0100

    add framework

---
+++ MyEmailValidator/MyEmailValidator/MyEmailValidator.h
@@ -0,0 +1,18 @@
+//
+//  MyEmailValidator.h
+//  MyEmailValidator
+//
+//  Created by Wojciech Spaleniak on 02/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyEmailValidator.
+FOUNDATION_EXPORT double MyEmailValidatorVersionNumber;
+
+//! Project version string for MyEmailValidator.
+FOUNDATION_EXPORT const unsigned char MyEmailValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyEmailValidator/PublicHeader.h>
+
+
